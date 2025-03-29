d4c6db656d678bdb103ee1edc10de96aba6c76d6
tarkue <mrfrostlikchannel@gmail.com>
2025-03-29 09:33:16 +0500

    add icons

--- src/widgets/Header/ui/header.tsx
+++ src/widgets/Header/ui/header.tsx
@@ -3,7 +3,7 @@ import Icon from '@/shared/ui/Icon';
 
 export default function Header() {
   return (
-    <header className="w-full flex items-center justify-center bg-gray-900 p-3">
+    <header className="w-full flex items-center justify-center bg-gray-900 p-3 ">
       <div className="max-w-[1100px] w-full flex items-center justify-between">
         <Icon glyph="logo" size="logo" />
         <Button variant="icon">
