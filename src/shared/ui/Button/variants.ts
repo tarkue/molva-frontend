5412c56b7d87c5542869186e97efe3e671705d58
tarkue <mrfrostlikchannel@gmail.com>
2025-04-07 21:33:22 +0500

    add border in circle-button

--- src/shared/ui/Button/variants.ts
+++ src/shared/ui/Button/variants.ts
@@ -8,7 +8,7 @@ const buttonVariants = cva(
         primary:
           'px-6 py-2 rounded-xl bg-blue text-white dark:text-black hover:bg-base-02 hover:text-blue active:bg-dark-blue active:text-white',
         circle:
-          'px-3 py-3 bg-base-03 border-base-08 hover:bg-base-05 rounded-full',
+          'px-3 py-3 bg-base-03 border border-base-08 hover:bg-base-05 rounded-full',
       },
       size: {
         large: 'w-full',
