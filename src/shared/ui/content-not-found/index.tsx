576861166f6c138db0a3476be31534b906c52c68
tarkue <mrfrostlikchannel@gmail.com>
2025-04-25 19:37:48 +0500

    add content-not-found element

---
+++ src/shared/ui/content-not-found/index.tsx
@@ -0,0 +1,7 @@
+export const ContentNotFound = ({ name }: { name: string }) => (
+  <div className="h-14 w-full">
+    <p className="w-full text-center text-subhead text-gray">
+      {name} не найдены
+    </p>
+  </div>
+);
--- src/widgets/review-list/index.tsx
+++ src/widgets/review-list/index.tsx
@@ -1,6 +1,7 @@
 import { ReviewCard } from '@/entity/reviews';
 import { useGetDisciplineReviews } from './api/reviews';
 import { Spinner } from '@/shared/ui/Spinner';
+import { ContentNotFound } from '@/shared/ui/content-not-found';
 
 const ReviewList = () => {
   const { data, isLoading, isError } = useGetDisciplineReviews();
@@ -8,9 +9,7 @@ const ReviewList = () => {
   if (isLoading) return <Spinner />;
 
   if (!data || data.length === 0 || isError)
-    return (
-      
-    );
+    return <ContentNotFound name="Отзывы" />;
 
   return (
     <ul className="flex flex-col gap-6">
