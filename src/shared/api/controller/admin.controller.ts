7b4209b6c5ae5896cc622b712ecfc360702ac5f6
tarkue <mrfrostlikchannel@gmail.com>
2025-05-07 20:34:31 +0500

    review admin controller

--- src/shared/api/controller/admin.controller.ts
+++ src/shared/api/controller/admin.controller.ts
@@ -1,36 +1,36 @@
 import client from '../base';
-import { Module, UpdateModuleDTO } from '../dto/module.dto';
+import { Module } from '../dto/module.dto';
 import { OnlyId } from '../dto/shared.dto';
-import { User } from '../dto/user.dto';
+import { SafeUser, User } from '../dto/user.dto';
 
 const BASE_URL = '/admin';
 
 export const add = async (dto: OnlyId) =>
-  await client.patch(`${BASE_URL}/add`, {
-    body: JSON.stringify(dto),
-  });
+  await client
+    .patch(`${BASE_URL}/add`, {
+      body: JSON.stringify(dto),
+    })
+    .json<SafeUser>();
 
 export const remove = async (dto: OnlyId) =>
-  await client.patch(`${BASE_URL}/admin/remove`, {
-    body: JSON.stringify(dto),
-  });
+  await client
+    .patch(`${BASE_URL}/admin/remove`, {
+      body: JSON.stringify(dto),
+    })
+    .json<SafeUser>();
 
 export const getAll = async () =>
   await client.get(`${BASE_URL}/admin/get`).json<User[]>();
 
 export const module = {
   get: async () =>
-    await client.get(`${BASE_URL}/modules/get`).json<Module[]>(),
+    await client
+      .get(`${BASE_URL}/modules/public/get`)
+      .json<Module[]>(),
   add: async (module: Module) =>
-    await client.post(`${BASE_URL}/module/add`, {
-      body: JSON.stringify(module),
-    }),
-  edit: async (module: UpdateModuleDTO) =>
-    await client.patch(`${BASE_URL}/module/edit`, {
-      body: JSON.stringify(module),
-    }),
-  remove: async (module: OnlyId) =>
-    await client.post(`${BASE_URL}/module/remove`, {
-      body: JSON.stringify(module),
-    }),
+    await client
+      .post(`${BASE_URL}/module/add`, {
+        body: JSON.stringify(module),
+      })
+      .json<Module>(),
 };
--- src/shared/api/dto/module.dto.ts
+++ src/shared/api/dto/module.dto.ts
@@ -2,8 +2,5 @@ import { OnlyId } from './shared.dto';
 
 export interface Module extends OnlyId {
   name: string;
-}
-
-export interface UpdateModuleDTO extends OnlyId {
-  new_name: string;
+  created_at: string;
 }
