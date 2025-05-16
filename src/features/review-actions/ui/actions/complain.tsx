1350383b5698aceec12fc6e8e9e53b01ec22f232
tarkue <mrfrostlikchannel@gmail.com>
2025-05-17 01:45:01 +0500

    add confirm on complain

--- src/features/review-actions/ui/actions/complain.tsx
+++ src/features/review-actions/ui/actions/complain.tsx
@@ -1,9 +1,20 @@
 import Icon from '@/shared/ui/icon';
 import { useComplainSubmit } from '../../api/complain';
 import { ReviewProps } from '../../models/review-props';
+import Button from '@/shared/ui/button';
+import { useOpenConfirmRemoveForm } from '@/features/open-confirm-remove';
 
 export const Complain = ({ review }: ReviewProps) => {
   const onSubmit = useComplainSubmit(review);
+  const open = useOpenConfirmRemoveForm({
+    onSubmit,
+    type: 'COMPLAIN',
+    text: 'Вы уверены, что хотите пожаловаться на данный отзыв? Это действие будет невозможно отменить.',
+  });
 
-  return <Icon glyph="report" stroke="red" onClick={onSubmit} />;
+  return (
+    <Button onClick={open}>
+      <Icon glyph="report" stroke="red" />
+    </Button>
+  );
 };
