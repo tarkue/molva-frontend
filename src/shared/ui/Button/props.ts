0b2454d10e830bd97953998328bd950a88bd84ad
Smirnov Evgenij <137773848+tarkue@users.noreply.github.com>
2025-05-24 23:50:26 +0500

    Delete src/shared/ui/Button directory

--- src/shared/ui/Button/props.ts
+++
@@ -1,6 +0,0 @@
-import { type VariantProps } from 'class-variance-authority';
-import { buttonVariants } from './variants';
-
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
