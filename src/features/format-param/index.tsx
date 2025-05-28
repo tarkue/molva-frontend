bdc4f1f5af0d9165cfa9ccd8b17bfbdc5337db09
tarkue <mrfrostlikchannel@gmail.com>
2025-05-28 17:43:10 +0500

    Update FormatParam component to allow undefined values in useSearchParam and enable clearing of selection with isClearable prop

--- src/features/format-param/index.tsx
+++ src/features/format-param/index.tsx
@@ -6,11 +6,12 @@ import { Select } from '@/shared/ui/select';
 const FormatPair = enumToPair(Format);
 
 export const FormatParam = () => {
-  const [format, setFormat] = useSearchParam<Format>('format');
+  const [format, setFormat] = useSearchParam<Format | undefined>('format');
   return (
     <fieldset className="w-full lg:w-[230px]">
       <Select
         placeholder="Формат проведения"
+        isClearable={true}
         aria-label="Выберите формат проведения"
         defaultInputValue={format}
         onChange={(e) => setFormat(e?.label as Format)}
