967124ea10e2ce793122c01461db81bbf75a5da9
tarkue <mrfrostlikchannel@gmail.com>
2025-05-29 02:12:13 +0500

    Enhance MainPageMeta to include dynamic theme color based on user preference, improving visual consistency and user experience.

--- src/pages/main/meta.tsx
+++ src/pages/main/meta.tsx
@@ -1,17 +1,26 @@
+import { useTheme } from '@/shared/models/theme-store';
+
 export const MainPageMeta = () => {
+  const { theme } = useTheme();
   return (
-    <script type="application/ld+json">
-      {JSON.stringify({
-        '@context': 'https://schema.org',
-        '@type': 'WebSite',
-        name: 'Molva',
-        alternateName: [
-          'Помощник в выборе дисциплин в УрФУ',
-          'Помощник урфу',
-          'Молва',
-        ],
-        url: import.meta.env.VITE_URL,
-      })}
-    </script>
+    <>
+      <meta
+        name="theme-color"
+        content={theme === 'light' ? '#8EAEFF' : '#121212'}
+      />
+      <script type="application/ld+json">
+        {JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'WebSite',
+          name: 'Molva',
+          alternateName: [
+            'Помощник в выборе дисциплин в УрФУ',
+            'Помощник урфу',
+            'Молва',
+          ],
+          url: import.meta.env.VITE_URL,
+        })}
+      </script>
+    </>
   );
 };
