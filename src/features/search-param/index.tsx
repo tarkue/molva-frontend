7c5d0dc48cb130ac7fd3f126b2f588e2e4c12199
tarkue <mrfrostlikchannel@gmail.com>
2025-05-28 17:48:16 +0500

    Refactor SearchParam component to ensure input value is set directly from search state and defaults to an empty string

--- src/features/search-param/index.tsx
+++ src/features/search-param/index.tsx
@@ -6,8 +6,8 @@ export const SearchParam = ({
 }: {
   placeholder?: string;
 }) => {
-  const [search, setSearch] = useSearchParam<string | undefined>('q', {
-    default: undefined,
+  const [search, setSearch] = useSearchParam<string>('q', {
+    default: '',
   });
 
   return (
@@ -18,7 +18,7 @@ export const SearchParam = ({
         placeholder={placeholder}
         aria-label="Поисковая строка"
         type="seach"
-        value={search || search == 'undefined' ? '' : search}
+        value={search}
         onChange={(e) => setSearch(e.currentTarget.value)}
       />
     </fieldset>
