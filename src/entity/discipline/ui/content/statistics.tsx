f8645fdf85032d87a1f763bfed9841816eb32514
tarkue <mrfrostlikchannel@gmail.com>
2025-06-18 01:28:17 +0500

    Adjust gap spacing in DisciplineContentStatistics component for improved layout on larger screens

--- src/entity/discipline/ui/content/statistics.tsx
+++ src/entity/discipline/ui/content/statistics.tsx
@@ -1,16 +1,16 @@
+import { zip } from '@/shared/lib/array-utils';
 import Icon from '@/shared/ui/icon';
 import {
   generateStatistic,
   StatisticProps,
   statisticTitles,
 } from '../../models/statistic';
-import { zip } from '@/shared/lib/array-utils';
 
 const DisciplineContentStatistics = (props: StatisticProps) => {
   const statistics = zip(statisticTitles, generateStatistic(props));
 
   return (
-    <ul className="flex gap-5 w-full flex-col md:flex-row">
+    <ul className="flex gap-3 lg:gap-5 w-full flex-col md:flex-row">
       {statistics.map(([title, el], key) => (
         <li
           key={key}
