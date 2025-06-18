332e3a799ed79ffe29f0c7c14dc00b1e7c58c746
tarkue <mrfrostlikchannel@gmail.com>
2025-06-18 11:31:45 +0500

    Enhance discipline data fetching by incorporating refresh state into the query key, improving data consistency on updates.

--- src/pages/discipline/api/index.ts
+++ src/pages/discipline/api/index.ts
@@ -1,9 +1,11 @@
 import { api } from '@/shared/api';
+import { useRefresh } from '@/shared/models/refresh-store';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router';
 
 const useGetDiscipline = () => {
   const params = useParams();
+  const { refreshState } = useRefresh();
 
   const id = params.id;
 
@@ -12,7 +14,7 @@ const useGetDiscipline = () => {
   }
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['discipline', id],
+    queryKey: ['discipline', id, refreshState],
     queryFn: () => api.discipline.get(id),
   });
 
